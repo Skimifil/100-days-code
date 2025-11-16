87c5b5bd175578a5c0baaa2d848ff8074ee65b9b
Skimifil <rafaelpinheiro_@hotmail.com>
2025-11-16 16:57:55 -0300

    Day 03 Add `devbox.json` for Go setup, update Day02 tasks program, and create `functions.go` for type demonstration

--- Day01-30/Day02/todolist.go
+++ Day01-30/Day02/todolist.go
@@ -4,11 +4,18 @@ import "fmt"
 
 func main() {
 	var golang_course = "Assistir ao curso de Golang da Nana do canal TechWorld With Nana"
+	var taskProjectGo = "Fazer um projeto em Go"
 	var reaward_after_finish = "Comer uma bolachinha"
 
-	fmt.Println("Lista de afazeres")
-	fmt.Println(golang_course)
-	fmt.Println("Parabéns, segue seu presente por ter terminado o curso")
-	fmt.Println(reaward_after_finish)
+	var tasksItems = []string{golang_course, taskProjectGo}
 
+	fmt.Println("### TAREFAS ###")
+	fmt.Println("Lista de afazeres")
+	for index, task := range tasksItems {
+		fmt.Printf("	- %d. %s\n", index+1, task)
+	}
+
+	fmt.Println("\n### RECOMPENSAS ###")
+	fmt.Println("Ao fim de finalizar todas as trefas, parabéns, segue seu presente por ter terminado o curso")
+	fmt.Println(reaward_after_finish)
 }
