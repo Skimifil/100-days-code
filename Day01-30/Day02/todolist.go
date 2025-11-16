e7bdf5150758d9998efed9000286f402ef3c2941
Skimifil <rafaelpinheiro_@hotmail.com>
2025-11-16 14:24:27 +0000

    Day 02

---
+++ Day01-30/Day02/todolist.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var golang_course = "Assistir ao curso de Golang da Nana do canal TechWorld With Nana"
+	var reaward_after_finish = "Comer uma bolachinha"
+
+	fmt.Println("Lista de afazeres")
+	fmt.Println(golang_course)
+	fmt.Println("Parabéns, segue seu presente por ter terminado o curso")
+	fmt.Println(reaward_after_finish)
+
+}
