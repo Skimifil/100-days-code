4a01201f9e331c2ea4ffea20c36159b4bd974660
Skimifil <rafaelpinheiro_@hotmail.com>
2025-11-21 19:27:58 -0300

    Estava com preguição no dia 3, não criei nada absurdo e pedi ajuda pra IA deixar bunitinho.

--- Day01-30/Day03/functions.go
+++ Day01-30/Day03/functions.go
@@ -4,6 +4,18 @@ import (
 	"fmt"
 )
 
+func sumNumbers(x, y int) int {
+	return x + y
+}
+
+func greetPerson(name string) string {
+	return fmt.Sprintf("Hello, %s! Welcome to Go functions!", name)
+}
+
 func main() {
-	fmt.Printf("%T\n", main)
+	result := sumNumbers(5, 3)
+	fmt.Printf("Sum of 5 and 3 is: %d\n", result)
+
+	greeting := greetPerson("Alice")
+	fmt.Println(greeting)
 }
